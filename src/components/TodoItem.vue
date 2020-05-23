758884a560136d338b92d644b65b9c7dcb797b93
moriyamasaaaki <moriya-7071@outlook.com>
2020-05-23 20:40:17 +0900

    feat(vue): 不要なコード削除v-forで反復処理

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,12 +1,26 @@
 <template>
     <article class="message">
       <div class="message-header">
-        <p>Vue</p>
+        <p>{{ title }}</p>
         <button class="delete" aria-label="delete"></button>
       </div>
       <div class="message-body">
-        このプロジェクトは、Web UI開発（コンポーネント、宣言的UI、ホットリロード、タイムトラベルデバッグなど）において、アイデアをより実現させることに焦点を当てている。独断的ではなく開発者がピックアップしやすい。
-        部分的に採用可能なアーキテクチャ（プログレッシブフレームワーク）を採用している。 コアライブラリは、宣言的なレンダリングとコンポーネントの構成に焦点を合わせ、既存のページに埋め込むことができる。 ルーティング、状態管理、ビルドツールなどの複雑なアプリケーションに必要な高度な機能は、正式に維持されているサポートライブラリとパッケージを介して提供される。
+          {{ description }}
       </div>
     </article>
 </template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+    }
+}
+</script>
