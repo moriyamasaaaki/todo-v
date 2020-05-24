df08f87353b1bd5576ff34197552a36c2392b5ac
moriyamasaaaki <moriya-7071@outlook.com>
2020-05-24 14:11:46 +0900

    feat(vue): todoの追加

--- src/components/TodoCreate.vue
+++ src/components/TodoCreate.vue
@@ -1,24 +1,26 @@
 <template>
-<modal>
-        <form action>
-          <div class="field">
-            <label class="label">タイトル</label>
-            <div class="control">
-              <input v-model="form.title" class="input" type="text" placeholder="Text input" />
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">内容</label>
-            <div class="control">
-              <input v-model="form.description" class="input" type="text" placeholder="Text input" />
-            </div>
-          </div>
-        </form>
-    </modal>
+  <modal>
+    <form action>
+      <div class="field">
+        <label class="label">タイトル</label>
+        <div class="control">
+          <input v-model="form.title" class="input" type="text" placeholder="Text input" />
+        </div>
+      </div>
+      <div>
+        <label class="label">内容</label>
+        <div class="control">
+          <textarea v-model="form.description" class="textarea" placeholder="10 lines of textarea" rows="10"></textarea>
+        </div>
+      </div>
+        <button @click="submitForm" type="button" class="button is-success">作成</button>
+        <button @click="isOpen = false" class="button">Cancel</button>
+    </form>
+  </modal>
 </template>
 
 <script>
-import Modal from '@/components/Modal'
+import Modal from "@/components/Modal";
 export default {
   components: {
     Modal
@@ -26,10 +28,20 @@ export default {
   data() {
     return {
       form: {
-        title: '',
-        description: ''
+        title: "",
+        description: ""
       }
-    }
+    };
   },
-}
+methods: {
+    submitForm() {
+      this.$emit('formSubmitted', {...this.form})
+      this.resetForm()
+    },
+    resetForm() {
+      this.form.title = ''
+      this.form.description = ''
+    }
+  }
+};
 </script>
